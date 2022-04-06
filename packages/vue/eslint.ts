3f8bce44e37e9b932616e97a6dacf1a092ce8348
人造人19号 <jiangang.zhang@jaderd.com>
2022-04-06 08:40:52 +0800

    chore(vue): update eslint config

--- packages/vue/eslint.ts
+++ packages/vue/eslint.ts
@@ -17,75 +17,8 @@ module.exports = {
     'plugin:compat/recommended',
     '@vue/typescript/recommended',
     'prettier',
+    '@vue/prettier',
   ],
-  settings: {
-    'prettier-vue': {
-      // Settings for how to process Vue SFC Blocks
-      SFCBlocks: {
-        /**
-         * Use prettier to process `<template>` blocks or not
-         *
-         * If set to `false`, you may need to enable those vue rules that are disabled by `eslint-config-prettier`,
-         * because you need them to lint `<template>` blocks
-         *
-         * @default true
-         */
-        template: true,
-
-        /**
-         * Use prettier to process `<script>` blocks or not
-         *
-         * If set to `false`, you may need to enable those rules that are disabled by `eslint-config-prettier`,
-         * because you need them to lint `<script>` blocks
-         *
-         * @default true
-         */
-        script: true,
-
-        /**
-         * Use prettier to process `<style>` blocks or not
-         *
-         * @default true
-         */
-        style: true,
-
-        // Settings for how to process custom blocks
-        customBlocks: {
-          // Treat the `<docs>` block as a `.markdown` file
-          docs: { lang: 'markdown' },
-
-          // Treat the `<config>` block as a `.json` file
-          config: { lang: 'json' },
-
-          // Treat the `<module>` block as a `.js` file
-          module: { lang: 'js' },
-
-          // Ignore `<comments>` block (omit it or set it to `false` to ignore the block)
-          comments: false,
-
-          // Other custom blocks that are not listed here will be ignored
-        },
-      },
-
-      // Use prettierrc for prettier options or not (default: `true`)
-      usePrettierrc: true,
-
-      // Set the options for `prettier.getFileInfo`.
-      // @see https://prettier.io/docs/en/api.html#prettiergetfileinfofilepath-options
-      fileInfoOptions: {
-        // Path to ignore file (default: `'.prettierignore'`)
-        // Notice that the ignore file is only used for this plugin
-        ignorePath: '.prettierignore',
-
-        // Process the files in `node_modules` or not (default: `false`)
-        withNodeModules: false,
-      },
-    },
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-  },
   plugins: [
     'import',
     'unicorn',
@@ -102,6 +35,7 @@ module.exports = {
     ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/prefer-default-export': 1,
     'import/no-duplicates': 2,
     'import/first': 0,
     'import/order': 2,
